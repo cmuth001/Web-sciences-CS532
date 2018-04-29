5946c21d6a799340f30d2e418d13f89c4afe3fd1
chandrasekha muthyala <cmuthyal@cs.odu.edu>
2018-04-29 02:22:41 -0400

    assignment8 with out report

---
+++ Assignments/A8/test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 27 00:45:20 2018
+
+@author: cmuthyal
+"""
+
+import docclass
+from subprocess import check_output
+
+cl = docclass.naivebayes(docclass.getwords)
+#remove previous db file
+# check_output(["rm", "chandu.db"])
+
+cl.setdb("SpamOrNot.db")
+# docclass.spamTrain(cl)
+# docclass.sampletrain(cl)
+docclass.checkSpamOrNot(cl)
+#classify text: "the banking dinner" as spam or not spam
+output = {'tp':0, 'tn':0, 'fp':0, 'fn':0}
+for i in range(1,11):
+    with open('./trainData/notSpam'+str(i)+'.txt') as f:
+          txt = f.read()
+          notSpamStatus = cl.classify(txt)
+          if notSpamStatus == 'Not Spam':
+          	output['tp']+=1
+          else:
+          	output['fn']+=1
+
+          print("Not Spam ",i,": ",notSpamStatus)
+          # print("Not_Spam ",x,": ",cl.classify(email), file=open('output.txt', 'a+'))
+
+    with open('./trainData/spam'+str(i)+'.txt') as f:
+          txt = f.read()
+          spamStatus = cl.classify(txt)
+          if spamStatus == 'Spam':
+          	output['tn']+=1
+          else:
+          	output['fp']+=1
+          print("Spam ",i,": ",spamStatus)
+print(output)
+print( cl.classify('You should now be able to use the alias ibhert@cs.odu.edu for communication. ') )
